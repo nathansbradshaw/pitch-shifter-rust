f9e0f8a3ff2c0a09b11a16bb9212ec2957ff314e
nathansbradshaw <nathansbradshaw@gmail.com>
2024-06-07 23:25:25 -0500

    test_2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod circular_buffer;
 
 use circular_buffer::CircularBuffer;
-use hound::{WavReader, WavSpec, WavWriter};
-use std::error::Error;
+use hound::{Sample, WavReader, WavSpec, WavWriter};
+use std::{error::Error, vec};
 
+const FFT_SIZE: usize = 512;
 const CHUNK_SIZE: usize = 1024;
 fn main() -> Result<(), Box<dyn Error>> {
-    let path = "sample.wav";
+    let path = "WeChooseToGoToTheMoon.wav";
     let mut reader = WavReader::open(path)?;
     let spec = reader.spec();
 
@@ -15,8 +16,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             match spec.bits_per_sample {
                 8 => read_and_write_samples::<i8>(&mut reader, &spec)?,
                 16 => read_and_write_samples::<i16>(&mut reader, &spec)?,
-                24 => read_and_write_samples::<i32>(&mut reader, &spec)?,
-                32 => read_and_write_samples::<i32>(&mut reader, &spec)?,
+                24 => return Err(Box::from("Unsupport bit depth 24")),
                 _ => return Err(Box::from("Unsupported bit depth")),
             }
         },
@@ -40,29 +40,30 @@ where
 {
     let output_path = "processed_sample.wav";
     let mut writer = WavWriter::create(output_path, *spec)?;
-    let mut buffer_in: CircularBuffer<i16> = CircularBuffer::new(0, Some(0), Some(0));
+    let mut buffer_in: CircularBuffer<f32> = CircularBuffer::new(0.0, Some(0), Some(0));
     let mut window_in = 0;
     let window_size = 256;
     let hop_size = 128;
-    let fft_size = 64;
     let mut hop_counter = 0;
-    let mut buffer_out: CircularBuffer<i16> = CircularBuffer::new(0, Some(hop_size), Some(0));
+    let mut buffer_out: CircularBuffer<f32> = CircularBuffer::new(0.0, Some(hop_size), Some(0));
 
-    for sample in reader.samples::<S>() {
-        let sample = sample?.as_i16();
+     let i16_samples: Vec<i16> = reader.samples::<i16>().map(|s| s.unwrap()).collect();
+    let f32_samples = convert_i16_to_f32(&i16_samples);
+
+    for sample in f32_samples {
         println!("Sample: {:?}", sample);
 
 
         buffer_in.write(sample);
         window_in += 1;
-        // if(window_in >= window_size) {
-        //     window_in = 0;
-        //     // PROCESSES DATA
-        //     println!("Window Size hit")
-        // }
+        if(window_in >= window_size) {
+            window_in = 0;
+            // PROCESSES DATA
+            println!("Window Size hit")
+        }
 
         let out_sample = buffer_out.read_and_reset();
-        // let scaled_out_sample = out_sample  * (hop_size as i16)/ fft_size;
+        let scaled_out_sample = out_sample  * (hop_size as f32)/ FFT_SIZE as f32;
         hop_counter += 1;
         if hop_counter >= hop_size {
             hop_counter = 0;
@@ -71,40 +72,59 @@ where
 
         }
 
-
-
-
-        // Here you can add your processing logic
-        let processed_sample = sample;
-
-        writer.write_sample(buffer_out.read())?;
+        writer.write_sample(convert_f32_array_to_i16(scaled_out_sample))?;
     }
 
     writer.finalize()?;
     Ok(())
 }
 
-fn process_fft( in_buffer: &mut CircularBuffer<i16>, out_buffer:&mut CircularBuffer<i16>) {
-    const FFT_SIZE: usize = 512;
+fn process_fft( in_buffer: &mut CircularBuffer<f32>, out_buffer:&mut CircularBuffer<f32>) {
     const BUFFER_SIZE: usize = 1024;
-    let analysis_window_buffer = [1; FFT_SIZE];
-    let mut unwrapped_buffer = [0.0; FFT_SIZE];
+    let analysis_window_buffer: [f32; FFT_SIZE] = hanning_window();
+     let mut unwrapped_buffer: [f32; FFT_SIZE] = [0.0; FFT_SIZE];
+    let mut fft_output_buffer: [microfft::Complex32; FFT_SIZE / 2 + 1] = [microfft::Complex32 { re: 0.0, im: 0.0 }; FFT_SIZE / 2 + 1];
 
-    // TODO FFT stuff for REAL
+
+    // Apply windowing and read from input buffer
     for n in 0..FFT_SIZE {
-        unwrapped_buffer[n] = (in_buffer.read() * analysis_window_buffer[n]) as f32; 
+        unwrapped_buffer[n] = in_buffer.read() * analysis_window_buffer[n] as f32;
     }
 
     let res = microfft::real::rfft_512(&mut unwrapped_buffer);
+    
+    // Process FFT output (e.g., calculate amplitude)
+    for (i, &complex) in fft_output_buffer.iter().enumerate() {
+        let amplitude = (complex.re.powi(2) + complex.im.powi(2)).sqrt(); // Calculate the amplitude
+        // println!("Frequency bin {}: amplitude {:?}", i, amplitude);
+    }
+    
 
     for n in res.iter().enumerate() {
         let amplitude = n;
-        println!("amplitude {:?}", amplitude);
+        // println!("amplitude {:?}", amplitude);
     }
 
     // TODO Inverse FFT
     let res = microfft::inverse::ifft_256( res);
     for n in res.iter().enumerate() {
-        out_buffer.write(n.1.re as i16)
+        out_buffer.write(n.1.re)
     }
+}
+// Function to generate a Hanning window
+fn hanning_window() -> [f32; FFT_SIZE] {
+    let mut window = [0.0; FFT_SIZE];
+    for n in 0..512 {
+        window[n] = 0.5 * (1.0 - (2.0 *  3.14 * n as f32 / (FFT_SIZE - 1) as f32).cos());
+    }
+    window
+}
+
+fn convert_i16_to_f32(samples: &[i16]) -> Vec<f32> {
+    samples.iter().map(|&sample| sample as f32).collect()
+}
+
+fn convert_f32_array_to_i16(value: f32) ->i16 {
+        let clamped_value = value.clamp(i16::MIN as f32, i16::MAX as f32);
+        clamped_value.round() as i16
 }
